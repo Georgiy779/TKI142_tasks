75b82e80c70c8c50d3a9413dabc31c4f8e1d0d14
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-24 20:23:29 +0300

    Задание 2.2

---
+++ task_2.2/task_2.2.cpp
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+* @brief точка входа в програму
+* @param x значение пераметра x
+* @param y значение пераметра y
+* @param a значение пераметра a
+* @return 0 в случве успеха
+*/
+
+double input();
+double output(double x, double a);
+
+int main()
+{
+	double a = 2.5, x, y;
+	x = input();
+	printf("x = %lf\n", x);
+	y = output(x, a);
+	printf("y = %lf\n", y);
+	return 0;
+}
+
+double input()
+{
+	double x;
+	printf("input x.\n");
+	scanf_s("%lf", &x);
+	return x;
+}
+
+double output(double x, double a)
+{
+	double y;
+	if (x > a)
+	{
+		y = x * pow(sin(x), 2);
+	}
+	else
+	{
+		y = x * pow(cos(x), 2);
+	}
+	return y;
+}
