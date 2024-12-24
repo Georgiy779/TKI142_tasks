26e9b18ad5b0705c6a5fdb4c6e36c85be9cff49b
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-24 19:48:54 +0300

    Задание 2.1

---
+++ task_2.1/task_2.1.cpp
@@ -0,0 +1,52 @@
+﻿#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <locale.h>
+
+int input();
+int outputc(int a, int b);
+int output(int a, int b);
+
+int main()
+{
+	double a, b;
+	a = input();
+	b = input();
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	outputc(a, b);
+	output(a, b);
+	return 0;
+}
+
+int input()
+{
+	int a;
+	printf("input a.\n");
+	scanf_s("%d", &a);
+	return a;
+}
+
+int outputc(int a, int b)
+{
+	setlocale(LC_ALL, "Russian");
+	int c;
+	c = a;
+	a = b;
+	b = c;
+	printf("Вывод с\n");
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	return 0;
+}
+
+int output(int a, int b)
+{
+	a ^= b ^= a ^= b;
+	printf("Вывод\n");
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	return 0;
+}
