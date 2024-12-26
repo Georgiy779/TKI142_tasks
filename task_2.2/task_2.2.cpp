83aa3d9c61018fd99dcfdf9a929ad0499343306d
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-26 14:45:31 +0300

    Исправление

--- task_2.2/task_2.2.cpp
+++ task_2.2/task_2.2.cpp
@@ -1,7 +1,24 @@
-﻿#include <stdio.h>
+﻿#define _USE_MATH_DEFINES
+#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+/**
+* @brief Ввод переменных x
+* @param x значение пераметра x
+* @return возвращение переменной x
+*/
+double input();
+
+/**
+* @brief точка входа в програму
+* @param x значение пераметра x
+* @param y значение пераметра y
+* @param a значение пераметра a
+* @return возвращение переменной y
+*/
+double output(double x, double a);
 
 /**
 * @brief точка входа в програму
@@ -10,13 +27,10 @@
 * @param a значение пераметра a
 * @return 0 в случве успеха
 */
-
-double input();
-double output(double x, double a);
-
 int main()
 {
-	double a = 2.5, x, y;
+	const double a = 2.5;
+	double x, y;
 	x = input();
 	printf("x = %lf\n", x);
 	y = output(x, a);
@@ -36,12 +50,7 @@ double output(double x, double a)
 {
 	double y;
 	if (x > a)
-	{
-		y = x * pow(sin(x), 2);
-	}
+		return x * pow(sin(x), 2);
 	else
-	{
-		y = x * pow(cos(x), 2);
-	}
-	return y;
+		return x * pow(cos(x), 2);
 }
