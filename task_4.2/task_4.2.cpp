dcadcb40e9e2f3a6d7a9f8f9ce1568c81bbdef0e
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-26 20:10:28 +0300

    Исправление

--- task_4.2/task_4.2.cpp
+++ task_4.2/task_4.2.cpp
@@ -5,54 +5,96 @@
 #include <float.h>
 #include <math.h>
 #include <locale.h>
-#include <time.h> // чтобы использовать функцию time()
+#include <time.h>
 
+/**
+* @brief Ввод массива
+* @param n значение количество элементов массива
+* @param f для определения способа ввода массива
+* @param array указатель на массив
+* @return array указатель на массив
+*/
+int* intmas(int n);
+
+/**
+* @brief Ввод массива
+* @param n значение количество элементов массива
+* @param min_val - значение минимального элемента массива
+* @param sred_elem_mas - средний элемент массива
+* @param min_index - индекс минимального элемента массива
+* @param array указатель на массив
+* @return 0
+*/
+int replacement_average(int* array, int n);
+
+/**
+* @brief Удаление всех элементов, в записи которых есть цифра 5
+* @param n значение количество элементов массива
+* @param arrayb указательвведёный для создание массива
+* @param str указательвведёный для создание массива строк
+* @param s значение количество элементов нового массива
+* @param array указатель на массив
+* @return возвращение массива arrayb
+*/
+int* delit(int* array, int n, int* s, char X);
+
+/**
+* @brief Удаление всех элементов, в записи которых есть цифра 5
+* @param n значение количество элементов массива
+* @param arrayb указательвведёный для создание массива
+* @param s значение количество элементов нового массива
+* @param array указатель на массив
+* @return возвращение массива arrayb
+*/
+int* intmasS(int* array, int n);
+
+/**
+* @brief Сформировать новый массив на основе существующего
+* @param n значение количество элементов массива
+* @param s значение количество элементов нового массива
+* @param array указатель на массив
+* @param arrayb указатель на новый массив
+* @return 0 в случве успеха или при неправильно введёной переменной n
+*/
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	setlocale(LC_NUMERIC, "en-US");
 
-	//n - количество элементов массива
-	//s - количество элементов нового массива
 	int n, s;
-
 	printf("Ведите n (не чётный).\n");
 	scanf_s("%d", &n);
-	printf("   -> %d\n\n", n);
 	if (n % 2 == 0)
 	{
 		printf("Неправильно введённая операция! \n");
 		return 0;
 	}
-	//Создание массива
+
 	int* array = intmas(n);
 
 	if (array != NULL)
 	{
-		//Вывод массива
 		printf("Исходный массив\n");
 		for (int i = 0; i < n; i++)
 			printf("array[%d] = %d\n", i, array[i]);
 
-		//Замена минимального элемента массива на средний.
-		zum_sred(array, n);
+		replacement_average(array, n);
+		for (int i = 0; i < n; i++)
+			printf("array[%d] = %d\n", i, array[i]);
 
-		//Удаление всех элементов, в записи которых есть цифра 5. 
 		printf("\nУдаление всех элементов, в записи которых есть цифра 5.\n");
 		int* arraya = delit(array, n, &s, '5');
 		for (int i = 0; i < s; i++)
 			printf("arraya[%d] = %d\n", i, arraya[i]);
 
-		//Сформировать новый массив на основе существующего.
 		printf("\nСформировать новый массив на основе существующего.\n");
-		int* arrayb = intmasb(array, n);
+		int* arrayS = intmasS(array, n);
 		for (int i = 0; i < n; i++)
-			printf("arrayb[%d] = %d\n", i, arrayb[i]);
+			printf("arrayb[%d] = %d\n", i, arrayS[i]);
 
-		//Освобождение памяти
 		free(array);
 		free(arraya);
-		free(arrayb);
+		free(arrayS);
 	}
 	else
 		printf("error");
@@ -60,60 +102,43 @@ int main()
 	return 0;
 }
 
-//Ввод массива
 int* intmas(int n)
 {
-	//для работы рандомайзера
 	srand(time(NULL));
 
-	//f - пераметр введённый для определения способа ввода массива
-	//i - индекс элемента массива
 	int f, i;
-	//array - указатель введённый для создания массива
 	int* array = NULL;
 
-	//Определение способа ввода массива
 	printf("1 - keybord\n2 - random\n");
 	scanf_s("%d", &f);
 	printf("   -> %d\n\n", f);
 
-	//Проверка правильного ввода переменной f
 	if (f == 1 || f == 2)
-		//Выделение памяти для массива
 		array = (int*)calloc(n, sizeof(int));
 	else
 		printf("Неправильно введённая операция! \n");
 
-	//Заполнение массива вручную
 	if (f == 1)
 	{
 		for (i = 0; i < n; i++)
 			scanf_s("%d", &array[i]);
 	}
 
-	//Заполнение массива случайными числами
 	if (f == 2)
 	{
 		for (i = 0; i < n; i++)
 			array[i] = rand();
 	}
 
-	//Возвращение массива
 	return array;
 }
 
-//Замена минимального элемента массива на средний.
-int zum_sred(int* array, int n)
+int replacement_average(int* array, int n)
 {
-	//min_val - значение минимального элемента массива
-	//sred_elem_mas - средний элемент массива
-	//min_index - индекс минимального элемента массива
 	int min_val, sred_elem_mas, min_index;
 
-	//Находим средний элемент массива
 	sred_elem_mas = (n - 1) / 2;
 
-	//Находим минимальный элемент массива, его значение и индекс
 	min_val = array[0];
 	min_index = 0;
 	for (int i = 0; i < n; i++)
@@ -125,35 +150,23 @@ int zum_sred(int* array, int n)
 		}
 	}
 
-	printf("Минимальный элемент массива = %d\n", array[min_index]);
+	printf("\nМинимальный элемент массива = %d\n", array[min_index]);
 	printf("Средний элемент массива = %d\n", array[sred_elem_mas]);
 
-	//Меняем местами средний и минимальный элемент массива
 	array[sred_elem_mas] ^= array[min_index] ^= array[sred_elem_mas] ^= array[min_index];
-
-	//Вывод обновлённого массива
-	for (int i = 0; i < n; i++)
-		printf("array[%d] = %d\n", i, array[i]);
 
 	return 0;
 }
 
-// удалить все элементы где есть цифра X
 int* delit(int* array, int n, int* s, char X)
 {
-	//arrayb - указательвведёный для создание массива
 	int* arrayb = NULL;
-	//Выделяем память для массив символов с максемальной длиной 20
 	char* str = (char*)calloc(20, sizeof(char));
 	*s = 0;
 
 	for (int i = 0; i < n; i++)
 	{
-		// преобразуем элемент массива array в строку
 		int sl = snprintf(str, sizeof str, "%d", array[i]);
-
-
-		//проверяем есть ли в строке символ X
 		int k = 0;
 		for (k = 0; k < sl; k++)
 		{
@@ -161,34 +174,23 @@ int* delit(int* array, int n, int* s, char X)
 				break;
 		}
 
-		//если k = sl в строке нету символа x, значит это значение надо записить в новый массив
 		if (k == sl)
 		{
-			//Увеличиваю количество элементов в новом массиве
 			*s = *s + 1;
-			//Выделяем память на ещё один элемент в массиве
 			arrayb = (int*)realloc(arrayb, *s * sizeof(int));
-			//Записываем элемент в новый массив
 			arrayb[*s - 1] = array[i];
 		}
 
 	}
-
-	//освобождение памяти
 	free(str);
-	//возвращение массива
 	return arrayb;
 }
 
-//Сформировать новый массив на основе существующего.
-int* intmasb(int* array, int n)
+int* intmasS(int* array, int n)
 {
-	//Указатель на нолвый элемент массива
 	int* arrayb = NULL;
-	//Выделение памяти под новый массив
 	arrayb = (int*)calloc(n, sizeof(int));
 
-	//Заполнение нового массива
 	for (int i = 0; i < n; i++)
 	{
 		if ((i + 1) % 2 == 0)
@@ -197,6 +199,5 @@ int* intmasb(int* array, int n)
 			arrayb[i] = 2 * array[i];
 	}
 
-	//Возвращение массива
 	return arrayb;
 }
