b2ffde0fc589fb5e845fad02f9cd43afbdfa2e0c
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-26 23:56:29 +0300

    Исправление

--- task_3.2.b/task_3.2.b.cpp
+++ task_3.2.b/task_3.2.b.cpp
@@ -6,24 +6,43 @@
 #include <math.h>
 
 /**
+* @brief расщёт факториала
+* @param k значение факториала
+* @return k
+*/
+double factorial(int n);
+
+/**
+* @brief ввод переменной
+* @param n значение вводимой переменной
+* @return n
+*/
+int input();
+
+/**
+* @brief вычисление сумму всех членов последовательности, не меньших заданного числа e
+* @param sum значение суммы членов последовательности
+* @param n переменная для вычисление сумму всех членов последовательности, не меньших заданного числа e
+* @param E значение члена последовательности
+* @return sum
+*/
+double sum(double e);
+
+/**
 * @brief точка входа в програму
-* @param n значение пераметра n
 * @param e значение пераметра e
-* @param k значение пераметра k
 * @return 0 в случве успеха
 */
-
-double factorial(int n);
-int input();
-double sum(int n, double e);
-
 int main()
 {
-	int n;
-	double e;
-	n = input();
+	double e, amount;
+
+	printf("input e\n");
 	e = input();
-	sum(n, e);
+
+	amount = sum(e);
+	printf("Sum = %lf\n", amount);
+
 	return 0;
 }
 
@@ -40,22 +59,24 @@ double factorial(int n)
 int input()
 {
 	int n;
-	printf("input n\n");
 	scanf_s("%d", &n);
 	return n;
 }
 
-double sum(int n, double e)
+double sum(double e)
 {
-	int i;
+	int i, n = 0;
 	double E = 0, sum = 0;
-	for (i = 1;i < n + 1; i++)
+	for (i = 1;n < 1000;i++ )
 	{
 		E = factorial(i) / factorial(i + 4);
-
 		if (E >= e)
+		{
 			sum = sum + E;
+			n--;
+		}
+		else
+			n++;
 	}
-	printf("Sum = %lf\n", sum);
-	return 0;
+	return sum;
 }
