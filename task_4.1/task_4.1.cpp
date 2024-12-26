b88f164a7519658305fadd542ceeab121cd5821b
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-26 17:21:44 +0300

    Исправления

--- task_4.1/task_4.1.cpp
+++ task_4.1/task_4.1.cpp
@@ -5,108 +5,115 @@
 #include <float.h>
 #include <math.h>
 #include <locale.h>
-#include <time.h> // чтобы использовать функцию time()
+#include <time.h>
 
+/**
+* @brief ввод одномерного массива
+* @param f репеменная для определения метода ввода массива
+* @param array указатель на массив из n членов
+* @param n количество членов массива
+* @return array указатель на массив
+*/
 int* intarray(int n);
+
+/**
+* @brief поиск суммы отрицательных элементов, значения которых кратно 10
+* @param sum сумму отрицательных элементов, значения которых кратно 10
+* @param array указатель на массив из n членов
+* @param n количество членов массива
+* @return значение переменной sum
+*/
 int sumMultiples(int* array, int n);
+
+/**
+* @brief Заменить первые k элементов массива на те же элементы в обратном порядке
+* @param с для изменения значений переменных массива
+* @param array указатель на массив из n членов
+* @param n количество членов массива
+* @return 0 в случве успеха
+*/
 int replacement(int* array, int n);
+
+/**
+* @brief Определяем, есть ли пара соседних элементов с произведением, равным заданному числу.
+* @param f значение заданого числа
+* @param array указатель на массив из n членов
+* @param n количество членов массива
+* @return 0 в случве успеха
+*/
 int Pair(int* array, int n);
 
+/**
+* @brief точка входа в програму
+* @param n значение количество переменных массива
+* @param array указатель на массив из n членов
+* @param sum сумму отрицательных элементов, значения которых кратно 10
+* @param array указатель на массив из n членов
+* @return 0 в случве успеха
+*/
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	setlocale(LC_NUMERIC, "en-US");
 
-	//n - количество элементов массива
-	//sum - Сумма элементов массива
 	int n, sum;
-
-	//Ввод количества элемнтов массива
 	printf("input n\n");
 	scanf_s("%d", &n);
-	printf("   -> %d\n\n", n);
 
-	//Создание массива
 	int* array = intarray(n);
-
-	//проверка на правильный ввод массива
 	if (array != NULL)
 	{
-		//Вывод массива
 		for (int i = 0; i < n; i++)
 		{
 			printf("array[%d] = %d\n", i, array[i]);
 		}
 
-		//Сумма отрицательных элементов массива кратных 10
-		sumMultiples(array, n);
+		sum = sumMultiples(array, n);
+		printf("sum = %d\n", sum);
 
-		//Заменить первые k элементов массива в обратном порядке
 		replacement(array, n);
 		for (int i = 0; i < n; i++)
 		{
 			printf("array[%d] = %d\n", i, array[i]);
 		}
 
-		//Определение пары элементов произведение которых равных заданному числу.
 		Pair(array, n);
 
-		//Освобождение памяти
 		free(array);
 	}
 	else
 		printf("error");
-
 	return 0;
 }
 
 int* intarray(int n)
 {
-	//Для работы раномайзера
 	srand(time(NULL));
-
-	//f - переменная введённая для определение способа ввода элементов
-	//i - индекс элемента массива
 	int f, i;
-	//array - указатель на массив
 	int* array = NULL;
-
-	//Определение способа ввода массива
 	printf("1 - keybord\n2 - random\n");
 	scanf_s("%d", &f);
 	printf("   -> %d\n\n", f);
-
-	//Проверка правильности ввода переменной f
 	if (f == 1 || f == 2)
-		//Выделение памяти для массива
 		array = (int*)calloc(n, sizeof(int));
 	else
 		printf("Неправильно введённая операция! \n");
-
-	//Заполнение массива с клавиатуры
 	if (f == 1)
 	{
 		for (i = 0; i < n; i++)
 			scanf_s("%d", &array[i]);
 	}
-
-	//Заполнение массива случайными значениями
 	if (f == 2)
 	{
 		for (i = 0; i < n; i++)
 			array[i] = rand();
 	}
-
-	//Возвращение массива
 	return array;
 }
 
-//Сумма отрицательных элементов массива кратных 10
 int sumMultiples(int* array, int n)
 {
-	//sum - сумма злементов
 	int sum = 0;
-	//Проверка на кратность 10 и отрицательность
 	for (int i = 0; i < n; i++)
 	{
 		if (array[i] < 0 && array[i] % 10 == 0)
@@ -114,24 +121,15 @@ int sumMultiples(int* array, int n)
 			sum = sum + array[i];
 		}
 	}
-
-	//Вывод суммы
-	printf("sum = %d\n", sum);
 	return 0;
 }
 
-//Заменить первые k элементов массива в обратном порядке
 int replacement(int* array, int n)
 {
-	//k - переменная обозначающая какое количество первых элемнтов должно быть записанно в обратном порядке
-	//с - переменная введённая для записи первых k элементов массива в обратном порядке
 	int c, k;
-	//Ввод переменной k
-	printf("input k\n");
+	printf("Ввидите количество первых k элементов\n");
 	scanf_s("%d", &k);
 	k--;
-	printf("   -> %d\n\n", k);
-	//запись первых k элементов в обратном порядке
 	for (int i = 0; i < k; i++)
 	{
 		c = array[i];
@@ -142,21 +140,15 @@ int replacement(int* array, int n)
 	return 0;
 }
 
-//Определение пары элементов произведение которых равных заданному числу.
 int Pair(int* array, int n)
 {
-	//f - заданное число
 	int f;
-	//Ввод переменной f
 	printf("input f\n");
 	scanf_s("%d", &f);
-	//Определение подходящей пары чисел
 	for (int i = 0; i < n - 1; i++)
 	{
 		if (array[i] * array[i + 1] == f)
-		{
 			printf(" array[%d] = %d and array[%d] = %d\n", i, array[i], i + 1, array[i + 1]);
-		}
 		else
 			printf("Таких элементов нет.\n");
 	}
