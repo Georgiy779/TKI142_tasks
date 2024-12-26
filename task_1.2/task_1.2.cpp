ff523755966d7c16e67875f92fcd9608b64c8bd7
Georgiy779 <georgiy.m.s@yandex.ru>
2024-12-26 14:37:48 +0300

    Исправление

--- task_1.2/task_1.2.cpp
+++ task_1.2/task_1.2.cpp
@@ -1,42 +1,60 @@
-﻿#define _USE_MATH_DEFINES
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <math.h>
 
 /**
+* @brief ввод значение координаты точки А или B
+* @param x значение координаты у точки А или B
+* @return значения x
+*/
+double input();
+
+/**
+* @brief расщёт расстояния от точки А до точки B
+* @param Ax значение пераметра "x" у точки А
+* @param Ay значение пераметра "y" у точки A
+* @param Bx значение пераметра "x" у точки B
+* @param By значение пераметра "y" у точки B
+* @param S значение расстояния от точки А до точки B
+* @return значения S
+*/
+double output(double Ax, double Ay, double Bx, double By);
+
+/**
 * @brief точка входа в програму
-* @param Ax значение пераметра x в точке А
-* @param Ay значение пераметра y в точке A
-* @param Bx значение пераметра x в точке B
-* @param By значение пераметра y в точке B
-* @param S значение переменной расстояния между точками
+* @param Ax значение пераметра "x" у точки А
+* @param Ay значение пераметра "y" у точки A
+* @param Bx значение пераметра "x" у точки B
+* @param By значение пераметра "y" у точки B
+* @param S значение переменной расстояния между точками А и B
 * @return 0 в случве успеха
 */
-
-int input(int x);
-double output(int Ax, int Ay, int Bx, int By);
-
 int main()
 {
-	int Ax = 0, Ay = 0, Bx = 0, By = 0;
-	Ax = input(Ax);
-	Ay = input(Ay);
-	Bx = input(Bx);
-	By = input(By);
-	output(Ax, Ay, Bx, By);
+	double Ax, Ay, Bx, By;
+	double S;
+	printf("Input Ax \n");
+	Ax = input();
+	printf("Input Ay \n");
+	Ay = input();
+	printf("Input Bx \n");
+	Bx = input();
+	printf("Input By \n");
+	By = input();
+	S = output(Ax, Ay, Bx, By);
+	printf("S = %f", S);
 	return 0;
 }
 
-int input(int x)
+double input()
 {
-
-	printf("Input \n");
-	scanf_s("%d", &x);
+	double x;
+	scanf_s("%lf", &x);
 	return x;
 }
 
-double output(int Ax, int Ay, int Bx, int By)
+double output(double Ax, double Ay, double Bx, double By)
 {
-	double S = sqrt(pow(Bx - Ax, 2) + pow(By - Ay, 2));
-	printf("S = %f", S);
-	return 0;
+	double S;
+	S = sqrt(pow(Bx - Ax, 2) + pow(By - Ay, 2));
+	return S;
 }
